29f3e7d08e130d9329c40faf3c033fdd2a4261b2
Влада Королева <ban-tyan@yandex.ru>
2025-09-25 08:17:06 +0500

    Добавлена структура point3d и базовый класс BoxRandomPoint

---
+++ lab1/box_random_point.h
@@ -0,0 +1,106 @@
+#ifndef BOX_RANDOM_POINT_H
+#define BOX_RANDOM_POINT_H
+
+#include <cstdlib>
+#include <ctime>
+
+/**
+ * @struct point3d
+ * @brief Структура для хранения трехмерных координат точки
+ */
+struct point3d {
+    double x;  ///< Координата X
+    double y;  ///< Координата Y
+    double z;  ///< Координата Z
+
+    /**
+     * @brief Конструктор инициализации точки
+     * @param x Координата X (по умолчанию 0.0)
+     * @param y Координата Y (по умолчанию 0.0)
+     * @param z Координата Z (по умолчанию 0.0)
+     */
+    point3d(double x = 0.0, double y = 0.0, double z = 0.0);
+    
+    /**
+     * @brief Вывод координат точки на экран
+     */
+    void print() const;
+    
+    /**
+     * @brief Получение координаты X
+     * @return Значение координаты X
+     */
+    double getBackX() const;
+    
+    /**
+     * @brief Получение координаты Y
+     * @return Значение координаты Y
+     */
+    double getBackY() const;
+    
+    /**
+     * @brief Получение координаты Z
+     * @return Значение координаты Z
+     */
+    double getBackZ() const;
+};
+
+/**
+ * @class BoxRandomPoint
+ * @brief Класс для генерации случайных точек в параллелепипеде
+ * 
+ * Класс позволяет генерировать случайные точки в заданной области,
+ * ограниченной параллелепипедом. Поддерживает возможность задания
+ * плоских областей (когда min == max по некоторой координате)
+ */
+class BoxRandomPoint {
+    double min_x;  ///< Минимальная координата X
+    double max_x;  ///< Максимальная координата X
+    double min_y;  ///< Минимальная координата Y
+    double max_y;  ///< Максимальная координата Y
+    double min_z;  ///< Минимальная координата Z
+    double max_z;  ///< Максимальная координата Z
+
+public:
+    /**
+     * @brief Конструктор инициализации границ области
+     * @param mx Минимальная координата X
+     * @param Mx Максимальная координата X
+     * @param my Минимальная координата Y
+     * @param My Максимальная координата Y
+     * @param mz Минимальная координата Z
+     * @param Mz Максимальная координата Z
+     * @warning Если min > max по любой координате, значения будут автоматически поменяны местами
+     */
+    BoxRandomPoint(double mx, double Mx, double my, double My, double mz, double Mz);
+    
+    /**
+     * @brief Установка новых границ области
+     * @param mx Минимальная координата X
+     * @param Mx Максимальная координата X
+     * @param my Минимальная координата Y
+     * @param My Максимальная координата Y
+     * @param mz Минимальная координата Z
+     * @param Mz Максимальная координата Z
+     */
+    void setBounds(double mx, double Mx, double my, double My, double mz, double Mz);
+    
+    /**
+     * @brief Генерация случайной точки в области
+     * @return Объект point3d со случайными координатами в заданных границах
+     */
+    point3d rnd();
+    
+    /**
+     * @brief Вывод текущих границ области на экран
+     */
+    void printBounds() const;
+    
+    /**
+     * @brief Сохранение параметров области в файл
+     * @param filename Имя файла для сохранения
+     */
+    void saveSet(const char* filename) const;
+};
+
+#endif
